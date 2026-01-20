7d3352fb9a7ecba3b961bd9a5085ebcfe589aae5
kalhara05 <kalhara139@gmail.com>
2026-01-20 22:05:21 +0530

    feat: implement Order service logic with stock management and transaction support
    - Created OrderRequest and OrderItemRequest DTOs for handling input data.
    - Implemented OrderServiceImpl with @Transactional annotation to ensure data integrity.
    - Added logic to validate product stock availability before placing an order.
    - Implemented automatic stock deduction upon successful order placement.
    - Added logic to calculate the total order amount dynamically.
    - Handled Customer and Product lookup validations.

---
+++ src/main/java/com/techstore/api/service/impl/OrderServiceImpl.java
@@ -0,0 +1,75 @@
+package com.techstore.api.service.impl;
+
+import com.techstore.api.dto.OrderItemRequest;
+import com.techstore.api.dto.OrderRequest;
+import com.techstore.api.model.Customer;
+import com.techstore.api.model.Order;
+import com.techstore.api.model.OrderItem;
+import com.techstore.api.model.Product;
+import com.techstore.api.repository.CustomerRepository;
+import com.techstore.api.repository.OrderRepository;
+import com.techstore.api.repository.ProductRepository;
+import com.techstore.api.service.OrderService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    @Override
+    @Transactional //Rollback if any issue
+    public Order placeOrder(OrderRequest orderRequest) {
+
+        Customer customer = customerRepository.findById(orderRequest.getCustomerId())
+                .orElseThrow(() -> new RuntimeException("Customer Not Found"));
+
+        Order order = new Order();
+        order.setCustomer(customer);
+        order.setTotalAmount(BigDecimal.ZERO);
+
+        List<OrderItem> orderItems = new ArrayList<>();
+        BigDecimal totalAmount = BigDecimal.ZERO;
+
+        for (OrderItemRequest itemRequest : orderRequest.getItems()) {
+
+            Product product = productRepository.findById(itemRequest.getProductId())
+                    .orElseThrow(() -> new RuntimeException("Product not found"));
+
+            if (product.getStockQuantity() < itemRequest.getQuantity()) {
+                throw new RuntimeException("Not enough stock for product: " + product.getName());
+            }
+
+            product.setStockQuantity(product.getStockQuantity() - itemRequest.getQuantity());
+            productRepository.save(product);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProduct(product);
+            orderItem.setQuantity(itemRequest.getQuantity());
+            orderItem.setPrice(product.getPrice());
+            orderItem.setOrder(order);
+
+            orderItems.add(orderItem);
+
+            BigDecimal lineAmount = product.getPrice().multiply(BigDecimal.valueOf(itemRequest.getQuantity()));
+            totalAmount = totalAmount.add(lineAmount);
+
+        }
+        order.setOrderItems(orderItems);
+        order.setTotalAmount(totalAmount);
+
+        return orderRepository.save(order);
+    }
+}
